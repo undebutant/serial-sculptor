bcc811b64f657c1a8fbf7fa95c9690f46a969500
undebutant <guillaume.gonthier@telecom-sudparis.eu>
2017-05-30 04:15:03 +0200

    Updating test package with the working path for the sfml libs, and setting up first sfml unit test

--- code-serial-sculptor/UnitTests/testsUnitaires.cpp
+++ code-serial-sculptor/UnitTests/testsUnitaires.cpp
@@ -1,14 +1,15 @@
+#include <SFML/Graphics.hpp>
+
 #include "gtest/gtest.h"
 #include "../MaBibliotheque/Scultor.h"
 #include "../MaBibliotheque/User.h"
-#include <SFML/Graphics.hpp>
 
 
 TEST(Exemple1, ScultorCreated)
 {
-	
-	
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-	EXPECT_FALSE(false);
-	
+	EXPECT_TRUE(shape.getRadius() == 100.f);
+	EXPECT_TRUE(shape.getFillColor() == sf::Color::Green);
 }
