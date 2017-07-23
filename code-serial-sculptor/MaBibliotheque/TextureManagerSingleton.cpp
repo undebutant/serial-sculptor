4cb9b9eb1abd503f1d186b69c07f0f299c6799b7
undebutant <guillaume.gonthier@telecom-sudparis.eu>
2017-07-23 22:09:19 +0100

    Commenting and clarifying the TextureManagerSingleton files

--- code-serial-sculptor/MaBibliotheque/TextureManagerSingleton.cpp
+++ code-serial-sculptor/MaBibliotheque/TextureManagerSingleton.cpp
@@ -1,11 +1,11 @@
 #include "TextureManagerSingleton.h"
-#include <iostream>
-#include <SFML/Graphics.hpp>
-
 
 using namespace std;
+using namespace sf;
 
+// Creating global instance singleton of the TextureManager
 TextureManagerSingleton TextureManagerSingleton::m_instance = TextureManagerSingleton();
+
 
 TextureManagerSingleton::TextureManagerSingleton()
 {
@@ -20,30 +20,145 @@ TextureManagerSingleton::~TextureManagerSingleton()
 	cout << "Texture Unloaded" << endl;
 }
 
+
+// Loading the font and various sprites needed ingame
+void TextureManagerSingleton::loadAll() {
+	Texture textureLoaded;
+
+	int const number = 11;
+
+	string pathToSprite = "sprite\\";
+	string nameForSprite;
+	string iteratorToString;
+	string variousImages[number] = { "chaton.png", "mainMenuBackground.jpeg", "exitButton.png","hardButton.png","hardOnButton.png",
+		"newGameButton.png" ,"gameBackground.jpeg","heartEmpty.png","heartFull.png","sculptor_left.png","sculptor_right.png" };
+	
+	// Loading the font used for the game
+	if (!loadFont("font\\SilentReaction.ttf")) {
+		cout << "Impossible de charger la police SilentReaction.ttf" << endl;
+	}
+	
+	// Loading various images whose name are stored in the variousImages vector
+	for (int i = 0; i < number; i++) {
+		if (!loadTexture(pathToSprite + variousImages[i], variousImages[i])) {
+			cout << "Impossible de charger la texture " << nameForSprite << endl;
+		}
+	}
+
+	// Loading all cloud bosses known
+	// TODO use xml to add / remove cloud bosses easily
+	for (int i = 1; i <= 6; i++) {
+		pathToSprite = "sprite\\";
+		nameForSprite = "CloudBoss";
+		iteratorToString = to_string(i);
+
+		nameForSprite = nameForSprite + iteratorToString + ".png";
+		pathToSprite = pathToSprite + nameForSprite;
+
+		if (!loadTexture(pathToSprite, nameForSprite)) {
+			cout << "Impossible de charger la texture " << nameForSprite << endl;
+		}
+	}
+
+	// Loading all cloud easy known
+	// TODO use xml to add / remove cloud easy easily
+	for (int i = 1; i <= 10; i++) {
+		pathToSprite = "sprite\\";
+		nameForSprite = "CloudEasy";
+		iteratorToString = to_string(i);
+
+		nameForSprite = nameForSprite + iteratorToString + ".png";
+		pathToSprite = pathToSprite + nameForSprite;
+
+		if (!loadTexture(pathToSprite, nameForSprite)) {
+			cout << "Impossible de charger la texture " << nameForSprite << endl;
+		}
+	}
+
+	// Loading keys sprite used above clouds
+	for (char i = 'A'; i <= 'Z'; i++) {
+		pathToSprite = "sprite\\";
+		nameForSprite = "";
+
+		nameForSprite.push_back(i);
+		nameForSprite = nameForSprite + ".png";
+		pathToSprite = pathToSprite + nameForSprite;
+
+		if (!loadTexture(pathToSprite, nameForSprite)) {
+			cout << "Impossible de charger la texture " << nameForSprite << endl;
+		}
+	}
+}
+
+
+
 TextureManagerSingleton& TextureManagerSingleton::Instance()
 {
 	return m_instance;
 }
 
+
+
+Texture* TextureManagerSingleton::getTexture(string texture) {
+	return &(textureLoaded[texture]);
+}
+
+
+Font TextureManagerSingleton::getFont() {
+	return fontLoaded;
+}
+
+
+
+int TextureManagerSingleton::getHeight() {
+	return height;
+}
+
+
+void TextureManagerSingleton::setHeight(int newHeight) {
+	height = newHeight;
+	heightsaved = newHeight;
+}
+
+
+
+int TextureManagerSingleton::getWidth() {
+	return width;
+}
+
+
+void TextureManagerSingleton::setWidth(int newWidth) {
+	width = newWidth;
+	widthsaved = newWidth;
+}
+
+
+
 bool TextureManagerSingleton::getFullscreen() {
 	return isFullscreen;
 }
 
+
 void TextureManagerSingleton::setFullscreen(int newHeight, int newWidth) {
 	height = newHeight;
 	width = newWidth;
-	isFullscreen = true;
 
+	isFullscreen = true;
 }
+
+
 void TextureManagerSingleton::setFullscreen(bool isFull) {
 	height = heightsaved;
 	width = widthsaved;
+
 	isFullscreen = false;
 }
 
 
-bool TextureManagerSingleton::loadTexture(std::string pathToTexture, std::string nameToStore) {
-	sf::Texture newTexture;
+
+// Loading a specific texture in memory with the following name, given the path
+bool TextureManagerSingleton::loadTexture(string pathToTexture, string nameToStore) {
+	Texture newTexture;
 
 	if (!newTexture.loadFromFile(pathToTexture)) {
 		return false;
@@ -55,8 +170,9 @@ bool TextureManagerSingleton::loadTexture(std::string pathToTexture, std::string
 }
 
 
-bool TextureManagerSingleton::loadFont(std::string pathToFont) {
-	sf::Font newFont;
+// Loading a specific font in memory given the path
+bool TextureManagerSingleton::loadFont(string pathToFont) {
+	Font newFont;
 
 	if (!newFont.loadFromFile(pathToFont)) {
 		return false;
@@ -65,86 +181,4 @@ bool TextureManagerSingleton::loadFont(std::string pathToFont) {
 		fontLoaded = newFont;
 		return true;
 	}
-}
-
-void TextureManagerSingleton::loadAll() {
-	if (!loadFont("font\\SilentReaction.ttf")) {
-		std::cout << "Impossible de charger la police SilentReaction.ttf" << std::endl;
-	}
-
-	sf::Texture textureLoaded;
-
-	std::string pathToSprite = "sprite\\";
-	std::string nameForSprite;
-	std::string iteratorToString;
-
-	int const number = 11;
-	std::string variousImages[number] = { "chaton.png", "mainMenuBackground.jpeg", "exitButton.png","hardButton.png","hardOnButton.png", "newGameButton.png" ,"gameBackground.jpeg","heartEmpty.png","heartFull.png","sculptor_left.png","sculptor_right.png" };
-
-	for (int i = 0; i < number; i++) {
-		if (!loadTexture(pathToSprite + variousImages[i], variousImages[i])) {
-			std::cout << "Impossible de charger la texture " << nameForSprite << std::endl;
-		}
-	}
-
-	for (int i = 1; i <= 6; i++) {
-		pathToSprite = "sprite\\";
-		nameForSprite = "CloudBoss";
-		iteratorToString = std::to_string(i);
-
-		nameForSprite = nameForSprite + iteratorToString + ".png";
-		pathToSprite = pathToSprite + nameForSprite;
-
-		if (!loadTexture(pathToSprite, nameForSprite)) {
-			std::cout << "Impossible de charger la texture " << nameForSprite << std::endl;
-		}
-	}
-
-	for (int i = 1; i <= 10; i++) {
-		pathToSprite = "sprite\\";
-		nameForSprite = "CloudEasy";
-		iteratorToString = std::to_string(i);
-
-		nameForSprite = nameForSprite + iteratorToString + ".png";
-		pathToSprite = pathToSprite + nameForSprite;
-
-		if (!loadTexture(pathToSprite, nameForSprite)) {
-			std::cout << "Impossible de charger la texture " << nameForSprite << std::endl;
-		}
-	}
-
-	for (char i = 'A'; i <= 'Z'; i++) {
-		pathToSprite = "sprite\\";
-		nameForSprite = "";
-
-		nameForSprite.push_back(i);
-		nameForSprite = nameForSprite + ".png";
-		pathToSprite = pathToSprite + nameForSprite;
-
-		if (!loadTexture(pathToSprite, nameForSprite)) {
-			std::cout << "Impossible de charger la texture " << nameForSprite << std::endl;
-		}
-	}
-}
-
-int TextureManagerSingleton::getHeight() {
-	return height;
-}
-int TextureManagerSingleton::getWidth() {
-	return width;
-}
-void TextureManagerSingleton::setHeight(int newHeight) {
-	height = newHeight;
-	heightsaved = newHeight;
-}
-void TextureManagerSingleton::setWidth(int newWidth) {
-	width = newWidth;
-	widthsaved = newWidth;
-}
-
-sf::Texture* TextureManagerSingleton::getTexture(std::string texture) {
-	return &(textureLoaded[texture]);
-}
-sf::Font TextureManagerSingleton::getFont() {
-	return fontLoaded;
 }
--- code-serial-sculptor/MaBibliotheque/TextureManagerSingleton.h
+++ code-serial-sculptor/MaBibliotheque/TextureManagerSingleton.h
@@ -1,33 +1,49 @@
 #pragma once
+
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
+
+
+
 class TextureManagerSingleton
 {
 public:
 	static TextureManagerSingleton& Instance();
+
 	sf::Texture* getTexture(std::string texture);
 	sf::Font getFont();
+
 	int getHeight();
-	int getWidth();
 	void setHeight(int newHeight);
+
+	int getWidth();
 	void setWidth(int newWidth);
+
+	bool getFullscreen();
 	void setFullscreen(int newHeight,int newWidth);
 	void setFullscreen(bool isFull);
-	bool getFullscreen();
 
 private:
-	
-	static TextureManagerSingleton m_instance;
-	std::map<std::string, sf::Texture> textureLoaded;
-	sf::Font fontLoaded;
-	int height;
-	int width;
-	int heightsaved;
-	int widthsaved;
 	bool isFullscreen;
 	bool loadTexture(std::string pathToTexture, std::string nameToStore);
 	bool loadFont(std::string pathToFont);
-	void loadAll();
+
+	int height;
+	int width;
+	int heightsaved;									// Last height used before fullscreen toggled on
+	int widthsaved;										// Last width used before fullscreen toggled on
+	
+	static TextureManagerSingleton m_instance;
+
+	sf::Font fontLoaded;
+
+	std::map<std::string, sf::Texture> textureLoaded;	// Map between the name given to a texture and the pointer to the loaded texture in RAM
+	
+
 	TextureManagerSingleton();
 	~TextureManagerSingleton();
+
+	void loadAll();
 };
 
